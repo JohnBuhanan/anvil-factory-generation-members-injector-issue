b14347b8b672daa40785db32e5f8109f8fc949bc
JohnBuhanan <johnbuns@yahoo.com>
2021-12-07 15:23:02 -0700

    wip

--- app/src/main/java/com/example/android/daggerwork/app/App.kt
+++ app/src/main/java/com/example/android/daggerwork/app/App.kt
@@ -1,15 +1,23 @@
 package com.example.android.daggerwork.app
 
 import android.app.Application
+import com.example.android.daggerwork.common.dagger.Components
+import com.example.android.daggerwork.feature.ui.ChatActivity
 import com.squareup.anvil.annotations.MergeComponent
 import javax.inject.Singleton
 
 @Suppress("UNCHECKED_CAST")
 class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
 
+//        val thing = DaggerAppComponent.create()
+//        Components.values.add(thing)
+    }
 }
 
 @[Singleton MergeComponent(Singleton::class)]
 interface AppComponent {
     fun inject(activity: SplashActivity)
+    fun inject(activity: ChatActivity)
 }
---
+++ common/src/main/java/com/example/android/daggerwork/common/dagger/Components.kt
@@ -0,0 +1,5 @@
+package com.example.android.daggerwork.common.dagger
+
+object Components {
+    val values = mutableListOf<Any>()
+}
