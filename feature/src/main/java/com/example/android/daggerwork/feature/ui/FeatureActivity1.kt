d0c4dba4ee3456ccce3a6895c63a606f61fcbb0d
JohnBuhanan <johnbuns@yahoo.com>
2021-12-07 16:00:00 -0700

    wip

--- feature/src/main/java/com/example/android/daggerwork/feature/ui/FeatureActivity1.kt
+++ feature/src/main/java/com/example/android/daggerwork/feature/ui/FeatureActivity1.kt
@@ -1,10 +1,10 @@
 package com.example.android.daggerwork.feature.ui
 
-import com.example.android.daggerwork.common.ui.BaseActivity1
+import com.example.android.daggerwork.common.ui.BaseActivity2
 import com.example.android.daggerwork.feature.dagger.F1
 import javax.inject.Inject
 
-abstract class FeatureActivity1 : BaseActivity1() {
+abstract class FeatureActivity1 : BaseActivity2() {
     @Inject
     lateinit var f1: F1
 }
